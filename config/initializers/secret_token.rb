1a3ae73ee96a6d05b63227a0d9a02bc7c076df74
vpablo <vpablo@appsource.biz>
2013-02-15 14:43:32 +0800

    Adds Bootstaps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EigthApp::Application.config.secret_token = 'e81c7be1114f06e76b383f7d530f5392a1ccfde755cc702cf821215ee8e2994cbafcad39eccc7da5721720f565286c7eff4892050382c25ccf713db42f45c71b'
